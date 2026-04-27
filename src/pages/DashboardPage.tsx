a2cf89c2238616c6cfdb02e97bf297c87ced8776
yuriEGV <yguajardov@gmail.com>
2026-04-27 15:00:53 -0400

    fix: Importado icono Briefcase faltante

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -4,7 +4,7 @@ import { usePermissions } from '../hooks/usePermissions';
 import { useTenant } from '../context/TenantContext';
 import api from '../services/api';
 import InstitutionalCalendar from '../components/InstitutionalCalendar';
-import { BookOpen, GraduationCap, Calendar, AlertCircle, FileText, School, MapPin, ShieldAlert, ChevronRight, Award, Clock, TrendingUp, User, X } from 'lucide-react';
+import { BookOpen, GraduationCap, Calendar, AlertCircle, FileText, School, MapPin, ShieldAlert, ChevronRight, Award, Clock, TrendingUp, User, X, Briefcase } from 'lucide-react';
 import EinsmartDashboardPage from './EinsmartDashboardPage';
 
 const DashboardPage = () => {
